789a9feb2fd806bafab4cd7e4b50895a0ed64157
DESKTOP-2554H7B\surva <survare@hotmail.com>
2017-07-06 12:52:00 +0300

    Укорочен код для класса клетки

--- src/lifecell.cpp
+++ src/lifecell.cpp
@@ -3,24 +3,14 @@
 LifeCell::LifeCell(const QRectF &rect, QGraphicsItem *parent)
     : QGraphicsRectItem(rect, parent)
 {
-    // При инициализации клетки - она мертвая
-    currentStatus = C_DIED;
+    // При запуске клетка изначально мертвая
+    changeStatus(C_DIED);
 }
 
 void LifeCell::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     // Если клетка живая была до нажатия, то перекрашиваем и делаем её мертвой
-    if (currentStatus == C_ALIVE)
-    {
-        this->setBrush(QBrush(Qt::white));
-        currentStatus   = C_DIED;
-    }
-    // Аналогично с мертвой клеткой
-    if (currentStatus == C_DIED)
-    {
-        this->setBrush(QBrush(Qt::black));
-        currentStatus   = C_ALIVE;
-    }
+    changeStatus((currentStatus == C_ALIVE) ? C_DIED : C_ALIVE);
 }
 
 CellStatus LifeCell::getStatus() const
@@ -33,7 +23,7 @@ void LifeCell::changeStatus(CellStatus stt)
     // Изменяем состояние и перекрашиваем
     currentStatus = stt;
     if (stt == C_ALIVE)
-        this->setBrush(Qt::white);
-    if (stt == C_DIED)
         this->setBrush(Qt::black);
+    if (stt == C_DIED)
+        this->setBrush(Qt::white);
 }
