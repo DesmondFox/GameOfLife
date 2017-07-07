bcf541b644e57e1f316754f7ef02db92f5670783
DESKTOP-2554H7B\surva <survare@hotmail.com>
2017-07-07 19:09:40 +0300

    Рисование на сцене
    Уже работает

--- src/lifescene.cpp
+++ src/lifescene.cpp
@@ -1,6 +1,85 @@
 #include "lifescene.h"
 
-LifeScene::LifeScene(QObject *parent) : QGraphicsScene(parent)
+
+LifeScene::LifeScene(ushort width, ushort height, QObject *parent) : QGraphicsScene(parent)
 {
+    // Установка начальных значений
+
+    // Интересно то, что надо немного уменьшить размер поля, т.к. вылезает полоса прокрутки.
+    // Костыль или нет - хз
+    sceneHeight = height-3;
+    sceneWidth  = width-3;
+    // TODO: решить проблему с полосой прокрутки
+
+    qDebug() << "Notice:\t Scene: Scene created with size [" << sceneWidth << " x " << sceneHeight << "]";
+
+    // Указателям присвоить nullptr
+    m_itemGroup = nullptr;
+
+    // Фон сцены - серый
+    this->addRect(0.0, 0.0, (qreal) sceneWidth, (qreal) sceneHeight,
+                  QPen(QColor(Qt::gray)), QBrush(Qt::gray));
+
+}
+
+void LifeScene::setField(size_t row, size_t col, unsigned short cellWidth)
+{
+    qDebug() << "Notice:\t Scene: Changing a field size as [" << row << " x " << col << "] " << cellWidth << "px";
+
+    Q_ASSERT(row > 0 && col > 0);
+    Q_ASSERT(cellWidth < MaxSizeOfCell);  // ограничение на размер клетки
+
+    this->rowCount  = row;
+    this->colCount  = col;
+    this->cellSize  = cellWidth;
+}
+
+void LifeScene::setCellSize(ushort width)
+{
+    // Проверим, подходит ли максимальному размеру
+    Q_ASSERT(width <= MaxSizeOfCell);
+
+    cellSize    = width;
+
+}
+
+void LifeScene::draw()
+{
+    //
+    // Рисовать клетки и добавлять в группу элементов
+    //
+
+    const ushort space = 1;
+
+    Q_ASSERT(rowCount > 0 && colCount > 0);
+
+    // Проверим на НЕсуществование группы элементов
+    if (m_itemGroup != nullptr)
+        return;
+
+    m_itemGroup = new QGraphicsItemGroup();
+    m_itemGroup->setHandlesChildEvents(false);
+    this->addItem(m_itemGroup);
+
+    LifeCell    *pCell  = nullptr;
+    for (size_t i = 0; i < rowCount; i++)
+    {
+        for (size_t j = 0; j < colCount; j++)
+        {
+            QRectF  rect;
+
+            // Вычисление координат клетки
+            qreal x  = j * cellSize;
+            qreal y  = i * cellSize;
+
+            rect.setX(x);               rect.setY(y);
+            rect.setWidth(cellSize);    rect.setHeight(cellSize);
+
+            pCell   = new LifeCell(rect);
+
+            m_itemGroup->addToGroup(pCell);
+        }
+    }
+
 
 }
--- src/lifescene.h
+++ src/lifescene.h
@@ -1,13 +1,43 @@
+//
+// Сцена для отрисовки клеток
+//
+
 #ifndef LIFESCENE_H
 #define LIFESCENE_H
 
-#include <QObject>
+#include <QGraphicsScene>
+#include <QGraphicsItemGroup>
+#include <QDebug>
 
-class LifeScene : public QObject
+#include "lifecell.h"
+
+class LifeScene : public QGraphicsScene
 {
     Q_OBJECT
 public:
-    explicit LifeScene(QObject *parent = nullptr);
+    explicit LifeScene(ushort width, ushort height, QObject *parent = nullptr);
+
+    // Загрузка размеров поля и клеток
+    void    setField(size_t row, size_t col, unsigned short cellWidth);
+    void    setCellSize(ushort width);
+
+    void    draw();
+
+private:
+    // максимальный размер клетки
+    static const ushort MaxSizeOfCell = 100;
+
+    // Количество ячеек (ряды/столбцы)
+    size_t rowCount;    // Количество рядов из клеток
+    size_t colCount;    // Количество столбцов из клеток
+    short  cellSize;    // Длинна стороны клетки
+
+    // Размер сцены
+    int sceneWidth;
+    int sceneHeight;
+
+    // Рисовка клеток
+    QGraphicsItemGroup *m_itemGroup;    // группа клеток (для удобства изменений)
 
 signals:
 
