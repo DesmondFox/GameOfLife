2a286593a2d00e19b43a96c8b989933c5342c058
DESKTOP-2554H7B\surva <survare@hotmail.com>
2017-07-15 21:53:57 +0300

    Багфикс. Неправильно очищало поле

--- src/graphicsitemmatrix.cpp
+++ src/graphicsitemmatrix.cpp
@@ -14,7 +14,9 @@ void GraphicsItemMatrix::allDead()
 {
     // Метод для очистки поля от живых клеток
 
-    for (size_t i = 0; i < this->countCol; i++)
-        for (size_t j = 0; j < this->countRow; j++)
-            m_matrix[i][j]->changeStatus(C_DIED);
+    for (size_t i = 0; i < this->countRow; i++)
+        for (size_t j = 0; j < this->countCol; j++)
+            this->m_matrix[i][j]->changeStatus(C_DIED);
+
+    qDebug() << "Notice:\t GraphicsItemMatrix: All cells cleared";
 }
